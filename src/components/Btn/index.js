91654c12196b6f7223cdff44c0d269169d6b6b48
Jiri Brabec <brabeji@gmail.com>
2015-12-06 14:44:32 +0100

    WIP 4

--- src/components/Btn/index.js
+++ src/components/Btn/index.js
@@ -1,16 +1,41 @@
 import React from 'react';
 import _ from 'lodash';
+import classNames from 'classnames';
+import { Link } from 'react-router';
 
 export default class Btn extends React.Component {
 
 	render() {
-		var { className, children, ...other } = this.props;
-		var newClassName = 'Btn ' + className;
-		return (
-			<button {...other} className={newClassName}>
-				{children}
-			</button>
-		);
+		var { className, children, modifiers, ...other } = this.props;
+		var classes = {
+			'Btn': true
+		};
+		if (className) {
+			classes[className] = true;
+		} else {
+			_.each(modifiers.split(/\s/i), (mod) => {
+				if (mod) {
+					classes['Btn--' + mod] = true;
+				}
+			});
+		}
+		var newClassName = classNames(classes);
+
+
+		return this.props.to ?
+			(<Link {...other} className={newClassName}>{children}</Link>)
+			:
+			(<a {...other} className={newClassName}>{children}</a>);
 	}
 
+
 }
+
+Btn.propTypes = {
+	modifiers: React.PropTypes.string
+};
+
+Btn.defaultProps = {
+	tagName: 'Link',
+	modifiers: ''
+};
